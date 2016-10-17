01a9cb67bead090f46cee496d3a2e06b7c783aa6
jcala <jcalaz@163.com>
2016-10-17 22:39:50 +0800

    去除了protobuffer

--- rest/src/main/java/xmarket/rest/ctrl/UserCtrl.java
+++ rest/src/main/java/xmarket/rest/ctrl/UserCtrl.java
@@ -1,14 +1,7 @@
 package xmarket.rest.ctrl;
 
-import xmarket.server.entity.transfer.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserCtrl {
-    @RequestMapping("/proto/read")
-     User.Register protoRead() {
-        return User.Register.newBuilder().setUsername("小王").setPassword("sdnskdks").setPhone("182003855656546").build();
-    }
 }
