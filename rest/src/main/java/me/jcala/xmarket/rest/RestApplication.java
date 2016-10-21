c73993460ccb1491ef4f08ec88e18b4ebeb7828d
jcala <jcalaz@163.com>
2016-10-22 03:15:55 +0800

    去除了resteasy，用springMVC做restful后端

--- rest/src/main/java/me/jcala/xmarket/rest/RestApplication.java
+++ rest/src/main/java/me/jcala/xmarket/rest/RestApplication.java
@@ -1,12 +1,8 @@
 package me.jcala.xmarket.rest;
 
 
-import org.springframework.stereotype.Component;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@Component
-@ApplicationPath("/rest/")
-public class RestApplication extends Application {
+//@Component
+//@ApplicationPath("/rest/")
+public class RestApplication {
 
 }
--- rest/src/main/java/me/jcala/xmarket/rest/ctrl/TestController.java
+++ rest/src/main/java/me/jcala/xmarket/rest/ctrl/TestController.java
@@ -1,16 +1,25 @@
 package me.jcala.xmarket.rest.ctrl;
 
+import io.swagger.annotations.*;
 import me.jcala.xmarket.pre.entity.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api("跟用户有关的api")
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/user")
 public class TestController {
 
-    @RequestMapping(value = "/test/user", method = RequestMethod.GET)
-    public User getUserInfoById(int id) {
+    @ApiOperation("获取用户信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType="path",name="id",dataType="int",required=true,value="Pet的ID",defaultValue="1")})
+    @ApiResponses({
+            @ApiResponse(code=400,message="请求参数没填好"),
+            @ApiResponse(code=404,message="请求路径没有或页面跳转路径不对")
+    })
+    @RequestMapping(value = "/echo", method = RequestMethod.GET)
+    public User getUserInfoById() {
         User user=new User();
         user.setUsername("jcala");
         user.setPassword("md5");
