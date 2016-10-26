a166b443b9aa735afb240a6eb5c008eec669cfda
jcala <jcalaz@163.com>
2016-10-27 02:19:09 +0800

    尝试morphia,准备用morphia替换spring data mongo

--- src/main/java/me/jcala/xmarket/server/ctrl/UserController.java
+++ src/main/java/me/jcala/xmarket/server/ctrl/UserController.java
@@ -2,6 +2,7 @@ package me.jcala.xmarket.server.ctrl;
 
 import io.swagger.annotations.*;
 import me.jcala.xmarket.server.entity.dto.Result;
+import me.jcala.xmarket.server.repository.UserRepo;
 import me.jcala.xmarket.server.service.inter.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+    @Autowired
+    private UserRepo userRepo;
 
     @Autowired
     private UserService userService;
@@ -30,7 +33,8 @@ public class UserController {
    @ApiOperation("用户注册")
    @PostMapping(value = "/register",produces="application/json;charset=UTF-8")
     public Result<String> register(String username,String password,String phone) throws RuntimeException{
-       return userService.register(username,password,phone);
+      // return userService.register(username,password,phone);
+      return userRepo.register();
    }
     @ApiOperation("设置用户学校信息")
     @PutMapping(value = "/school/{username}",produces="application/json;charset=UTF-8")
--- src/main/java/me/jcala/xmarket/server/entity/document/User.java
+++ src/main/java/me/jcala/xmarket/server/entity/document/User.java
@@ -2,22 +2,24 @@ package me.jcala.xmarket.server.entity.document;
 
 
 import lombok.*;
-import org.springframework.data.annotation.Id;
+import org.mongodb.morphia.annotations.*;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
-
 /**
  *用户信息封装类
  */
+@Entity("user")
 @Document(collection = "user")
 @Getter
 @Setter
 @NoArgsConstructor
+@Indexes(
+        @Index(value = "username", fields = @Field("username"))
+)
 public class User {
     @Id
     private String username;//用户名
     private String password;//用户密码
     private String school;//所在学校名称
     private String phone;//手机号
-    @Field("avatar_url") private String avatar_url;//头像地址
+    private String avatar_url;//头像地址
 }
