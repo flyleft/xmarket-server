b5421cd3b4b3bcec4c29398bded6c6b8081b5d3e
jcala <jcalaz@163.com>
2016-10-31 20:34:21 +0800

    添加了应用名前缀

--- src/main/java/me/jcala/xmarket/server/ctrl/SchoolController.java
+++ src/main/java/me/jcala/xmarket/server/ctrl/SchoolController.java
@@ -18,6 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/school")
 public class SchoolController {
+
     private SystemService schoolService;
 
     @Autowired
--- src/main/java/me/jcala/xmarket/server/ctrl/UserInfoController.java
+++ src/main/java/me/jcala/xmarket/server/ctrl/UserInfoController.java
@@ -8,6 +8,7 @@ import me.jcala.xmarket.server.entity.dto.Result;
 import me.jcala.xmarket.server.service.inter.StaticService;
 import me.jcala.xmarket.server.service.inter.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,36 +36,36 @@ public class UserInfoController {
             @ApiResponse(code=400,message="请求参数没填好"),
             @ApiResponse(code=404,message="请求路径没有或页面跳转路径不对")
     })
-    @PostMapping(value = "/login",produces="application/json;charset=UTF-8")
+    @PostMapping(value = "/login",produces= MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Result<String> login(String username,String password) throws RuntimeException{
         return userService.login(username,password);
     }
 
    @ApiOperation("用户注册")
-   @PostMapping(value = "/register",produces="application/json;charset=UTF-8")
+   @PostMapping(value = "/register",produces= MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Result<String> register(String username,String password,String phone)
            throws RuntimeException{
        return userService.register(username,password,phone);
    }
     @ApiOperation("设置用户学校信息")
-    @PutMapping(value = "/school",produces="application/json;charset=UTF-8")
+    @PutMapping(value = "/school",produces= MediaType.APPLICATION_JSON_UTF8_VALUE)
    public Result<String> updateUserSchool(String username,String school){
        return userService.updateSchool(username,school);
    }
     @ApiOperation("获取学校名称列表")
-    @GetMapping(value = "/school_list",produces = "application/json;charset=UTF-8")
+    @GetMapping(value = "/school_list",produces= MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Result<List<String>> getSchoolNameList() throws RuntimeException{
         return userService.gainSchoolList();
     }
 
     @ApiOperation("修改用户密码")
-    @PutMapping(value = "/pass",produces = "application/json;charset=UTF-8")
+    @PutMapping(value = "/pass", produces= MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Result<String> updateUserPassword(String username,String oldPass,String newPass)
             throws RuntimeException{
         return userService.updatePassword(username,oldPass,newPass);
     }
     @ApiOperation("修改用户头像")
-    @PutMapping(value = "/avatar",produces = "application/json;charset=UTF-8")
+    @PutMapping(value = "/avatar",produces= MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Result<String> updateUserAvatar(String username, HttpServletRequest request)
             throws RuntimeException{
       return userService.updateAvatar(username,request);
