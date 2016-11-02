d7d6cf61bd13b69b2547a555cd0d64caf8469817
jcala <jcalaz@163.com>
2016-11-02 19:47:19 +0800

    加入了spring security做权限引擎

---
+++ src/main/java/me/jcala/xmarket/server/conf/WebSecurityConfig.java
@@ -0,0 +1,16 @@
+package me.jcala.xmarket.server.conf;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Spring security的一些配置
+ */
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
+}
