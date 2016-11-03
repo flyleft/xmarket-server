f5d60ec89a3e468cc9aaf3e67291b7f1d5a5d20d
jcala <jcalaz@163.com>
2016-11-03 10:55:59 +0800

    给sys集合的name列加入了唯一性索引

--- src/main/java/me/jcala/xmarket/server/admin/entity/SystemBean.java
+++ src/main/java/me/jcala/xmarket/server/admin/entity/SystemBean.java
@@ -3,6 +3,7 @@ package me.jcala.xmarket.server.admin.entity;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 public class SystemBean {
     @Id
     private String id;
+    @Indexed(unique = true)
     private String name;//唯一不可重复
     private List<String> schools;
     private Authority authority;
