d8fd48d9d191c81778af3b6e0df281c6994961d5
jcala <jcalaz@163.com>
2016-11-07 02:04:18 +0800

    修正School和UserTrade中HTTP实体没有添加数据的错误

--- src/main/java/me/jcala/xmarket/server/service/SchoolServiceImpl.java
+++ src/main/java/me/jcala/xmarket/server/service/SchoolServiceImpl.java
@@ -15,8 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
