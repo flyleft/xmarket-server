2ad743bfeb8f1f88d75523f39e1a7a237a056285
jcala <jcalaz@163.com>
2016-11-15 00:05:26 +0800

    修改与学校有关的api路径

--- src/main/java/me/jcala/xmarket/server/ctrl/SchoolController.java
+++ src/main/java/me/jcala/xmarket/server/ctrl/SchoolController.java
@@ -28,13 +28,13 @@ public class SchoolController {
     }
 
     @ApiOperation(value = "获取学校名称列表",response = Result.class,produces = "application/json;charset=UTF-8")
-    @GetMapping(value = "/get",produces= MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @GetMapping(value = "/names/get",produces= MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<?> gainSchoolList(){
         return schoolService.getSchoolList();
     }
 
     @ApiOperation(value = "获取学校商品列表",response = Result.class,produces = "application/json;charset=UTF-8")
-    @GetMapping(value = "/{school_name}/trades/get",produces= MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @GetMapping(value = "/trades/{school_name}/get",produces= MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<?> gainSchoolTradeList(@PathVariable("school_name") String schoolName){
 
         return schoolService.getSchoolTradeList(schoolName);
