2d05a3551f6cb40fbea6409f6ba616d4992eef4f
jcala <jcalaz@163.com>
2016-11-22 22:53:22 +0800

    完成了sys表中tradeTags数据中pull新元素的操作

--- src/main/java/me/jcala/xmarket/server/admin/repository/AdminCustomRepository.java
+++ src/main/java/me/jcala/xmarket/server/admin/repository/AdminCustomRepository.java
@@ -1,4 +1,7 @@
 package me.jcala.xmarket.server.admin.repository;
 
+import me.jcala.xmarket.server.admin.entity.TradeTag;
+
 public interface AdminCustomRepository {
+   void addTradeTag(TradeTag tradeTag);
 }
--- src/main/java/me/jcala/xmarket/server/admin/repository/AdminCustomRepositoryImpl.java
+++ src/main/java/me/jcala/xmarket/server/admin/repository/AdminCustomRepositoryImpl.java
@@ -1,7 +1,31 @@
 package me.jcala.xmarket.server.admin.repository;
 
+import me.jcala.xmarket.server.admin.entity.SystemBean;
+import me.jcala.xmarket.server.admin.entity.TradeTag;
+import me.jcala.xmarket.server.admin.init.SysColName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+
 @Repository
-public class AdminCustomRepositoryImpl {
+public class AdminCustomRepositoryImpl implements AdminCustomRepository{
+
+    private MongoTemplate template;
+
+    @Autowired
+    public AdminCustomRepositoryImpl(MongoTemplate template) {
+        this.template = template;
+    }
+
+    @Override
+    public void addTradeTag(TradeTag tradeTag) {
+        template.updateFirst(new Query(where("name").is(SysColName.colTradeTag.name())),
+                new Update().pull("tradeTags",tradeTag), SystemBean.class);
+
+    }
+
 }
