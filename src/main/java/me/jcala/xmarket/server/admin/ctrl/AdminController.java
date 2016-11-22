305c93871f7db148ba5883810037a3724a111265
jcala <jcalaz@163.com>
2016-11-22 22:36:28 +0800

    AdminController中增加了添加商品分类的方法

--- src/main/java/me/jcala/xmarket/server/admin/ctrl/AdminController.java
+++ src/main/java/me/jcala/xmarket/server/admin/ctrl/AdminController.java
@@ -1,9 +1,12 @@
 package me.jcala.xmarket.server.admin.ctrl;
 
 import io.swagger.annotations.Api;
+import me.jcala.xmarket.server.admin.entity.TradeTag;
 import me.jcala.xmarket.server.admin.service.inter.SystemService;
 import me.jcala.xmarket.server.entity.dto.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,15 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
+
     private SystemService service;
+
 
     @Autowired
     public AdminController(SystemService service) {
         this.service = service;
     }
 
-    public Result<String> initSystem(){
+
+    @GetMapping(value = "/tags/add/update")
+    ResponseEntity<?> addTag(TradeTag tag){
         return null;
     }
+
 
 }
