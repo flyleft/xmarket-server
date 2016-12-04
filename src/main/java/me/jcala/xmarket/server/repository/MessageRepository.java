7cc82184d6c8e465d77bf39f7f9aaa2c938634fc
jcala <jcalaz@163.com>
2016-12-04 19:12:52 +0800

    添加了MessageRepository

---
+++ src/main/java/me/jcala/xmarket/server/repository/MessageRepository.java
@@ -0,0 +1,10 @@
+package me.jcala.xmarket.server.repository;
+
+import me.jcala.xmarket.server.entity.document.Message;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends MongoRepository<Message,String> {
+
+}
